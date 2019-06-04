6e5f4ae4728d114d044eae8f140e76e4178c8d64
Rajat <draj123258@gmail,com>
2019-06-04 12:19:35 +0530

    This is Shared

---
+++ app/src/main/java/com/bestmeme/memeswala/Fragments/Category.java
@@ -0,0 +1,29 @@
+package com.bestmeme.memeswala.Fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bestmeme.memeswala.R;
+
+public class Category extends Fragment {
+
+    View view;
+
+    public Category() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_category, container, false);
+        return view;
+    }
+}
